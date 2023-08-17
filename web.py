09b89a466069d44ff676f6dd13c6ee837410d9ac
Shaun <133212261+ShaunTheGeek@users.noreply.github.com>
2023-08-17 22:15:54 +0800

    feat: #2 search by keyword

--- web.py
+++ web.py
@@ -2,7 +2,7 @@ import streamlit as st
 from sqlalchemy.sql import text
 
 # Create the SQL connection to pets_db as specified in your secrets file.
-conn = st.experimental_connection('kb_db', type='sql', ttl=0)
+conn = st.experimental_connection('kb_db', type='sql')
 
 # Insert some data with conn.session.
 with conn.session as s:
@@ -11,22 +11,37 @@ with conn.session as s:
        title           TEXT    NOT NULL,
        content         TEXT     NOT NULL);'''))
 
-with st.form("import document"):
-    title = st.text_input('Title')
-    content = st.text_area('Content')
+tab1, tab2 = st.tabs(["Search", "Import"])
 
-    # Every form must have a submit button.
-    submitted = st.form_submit_button("Save")
-    if submitted:
-        with conn.session as s:
-            s.execute(text('''INSERT INTO documents (title, content)
-                            VALUES (:title, :content);'''),
-                      params=dict(title=title, content=content)
-                      )
-            s.commit()
+with tab1:
+    with st.form("search"):
+        keyword = st.text_input('keyword')
 
-# Query and display the data you inserted
-documents = conn.query(
-    'SELECT * FROM documents ORDER BY id desc LIMIT 10;',
-    ttl=0)
-st.dataframe(documents)
+        # Every form must have a submit button.
+        submitted = st.form_submit_button("Search")
+        if submitted:
+            documents = conn.query('SELECT * FROM documents WHERE content LIKE "%'
+                                   + keyword + '%" ORDER BY id desc LIMIT 10;',
+                                   ttl=0)
+            st.dataframe(documents)
+
+with tab2:
+    with st.form("import document"):
+        title = st.text_input('Title')
+        content = st.text_area('Content')
+
+        # Every form must have a submit button.
+        submitted = st.form_submit_button("Save")
+        if submitted:
+            with conn.session as s:
+                s.execute(text('''INSERT INTO documents (title, content)
+                                VALUES (:title, :content);'''),
+                          params=dict(title=title, content=content)
+                          )
+                s.commit()
+
+    # Query and display the data you inserted
+    documents = conn.query(
+        'SELECT * FROM documents ORDER BY id desc LIMIT 10;',
+        ttl=0)
+    st.dataframe(documents)
